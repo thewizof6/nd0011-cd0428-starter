fb8d3e6cfe9291819702b8902db7d1f0dc1f13c1
Darin_Thompson <thewizof6@gmail.com>
2025-09-06 12:15:18 -0700

    Completed json fetch in aboutMe section, required required update to image path in aboutMeData.json

--- starter/js/scripts.js
+++ starter/js/scripts.js
@@ -9,25 +9,67 @@ const mainElement = document.querySelector('main');
 mainElement.classList.add('main');
 
 const aboutMeFragment = document.createDocumentFragment()
-const fetchAboutMe = async() => {
-    const responseData = await fetch("starter/data/aboutMeData.json");
-    const aboutMeData = await responseData.json();
-    const newElement = document.createElement('p');
-    newElement.append("some Text");  
-    aboutMeFragment.append(newElement);
-    console.log(aboutMeFragment)
+const elementAboutMe = document.querySelector('div#aboutMe');
+
+
+
+
+const pathAboutMe = "starter/data/aboutMeData.json";
+
+const getAboutMe = async() => {
+    try {
+        const response = await fetch(pathAboutMe);
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+        const result = await response.json();        
+        const about = {...result};
+        const aboutParagraph = document.createElement('p');
+        aboutParagraph.textContent = result.aboutMe;
+        //console.log(about.aboutMe);
+        const imgDiv = document.createElement('div');
+        const aboutImage = document.createElement('img');
+        aboutImage.src = about.headshot;
+        aboutImage.alt = "Headshot of me";
+        elementAboutMe.append(aboutParagraph);
+        imgDiv.append(aboutImage);
+        elementAboutMe.append(imgDiv);
+        //console.log(aboutMeFragment);
+  
+   
+    
+    }   catch(error) {
+        console.error(error.message);
+    }
+
 }
+getAboutMe();
+//elementAboutMe.append(aboutMeFragment);
+//console.log(aboutMeFragment);
+console.log(elementAboutMe);
+/* function successCallBack(result) {
+    console.log(`Success! \n ${result}`);
+}
+function errorCallback(error) {
+    console.log(`Error: ${error}`);
+}
+ */
+/* const dataAboutMe = getData(pathAboutMe).then(successCallBack, errorCallback);
+console.log(dataAboutMe);
+ */
+
+/* 
 const elementAboutMe = document.querySelector('#aboutMe');
 const aboutParagraph = document.createElement('p');
 const aboutDiv = document.createElement('div');
 aboutDiv.classList.add('#headshotContainer');
-const aboutImage = document.createElement('img');
-aboutImage.src = "starter/images/headshot.webp";
+
+aboutImage.src = dataAboutMe.headShot;
 aboutImage.alt = "Headshot of me";
 aboutDiv.append(aboutImage);
 elementAboutMe.append(aboutParagraph);
-elementAboutMe.append(aboutDiv);
 
+ */
 
 
 
