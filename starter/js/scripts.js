03370651601a4dc4ee11847bb00392a2895d9b5f
Darin_Thompson <thewizof6@gmail.com>
2025-09-06 13:27:50 -0700

    code cleanup on header and aboutMe sections

--- starter/js/scripts.js
+++ starter/js/scripts.js
@@ -1,21 +1,28 @@
+//HEADER updates
+
+//add class to body
 const bodyClass = document.querySelector('body');
 bodyClass.classList.add('body');
+
+//add class to header and populate header element
 const headerElement = document.querySelector('header');
 headerElement.classList.add('header');
 headerElement.firstElementChild.textContent = 'Mississippi Flash';
+
+//add class to mavigatio menu
 const headerNav = document.querySelector('nav');
 headerNav.classList.add('nav');
+
+//MAIN updates
+
+//add call to main
 const mainElement = document.querySelector('main');
 mainElement.classList.add('main');
 
-const aboutMeFragment = document.createDocumentFragment()
+//Populate ABOUTME Container
 const elementAboutMe = document.querySelector('div#aboutMe');
-
-
-
-
 const pathAboutMe = "starter/data/aboutMeData.json";
-
+//fetch aboutMeData.json
 const getAboutMe = async() => {
     try {
         const response = await fetch(pathAboutMe);
@@ -24,57 +31,25 @@ const getAboutMe = async() => {
         }
         const result = await response.json();        
         const about = {...result};
+        //create and populate aboutMe element
         const aboutParagraph = document.createElement('p');
         aboutParagraph.textContent = result.aboutMe;
-        //console.log(about.aboutMe);
+        //create and populate portrait 
         const imgDiv = document.createElement('div');
         const aboutImage = document.createElement('img');
         aboutImage.src = about.headshot;
         aboutImage.alt = "Headshot of me";
+        //append new element to aboutMe Section
         elementAboutMe.append(aboutParagraph);
         imgDiv.append(aboutImage);
         elementAboutMe.append(imgDiv);
-        //console.log(aboutMeFragment);
-  
-   
-    
+
     }   catch(error) {
         console.error(error.message);
     }
 
 }
 getAboutMe();
-//elementAboutMe.append(aboutMeFragment);
-//console.log(aboutMeFragment);
-console.log(elementAboutMe);
-/* function successCallBack(result) {
-    console.log(`Success! \n ${result}`);
-}
-function errorCallback(error) {
-    console.log(`Error: ${error}`);
-}
- */
-/* const dataAboutMe = getData(pathAboutMe).then(successCallBack, errorCallback);
-console.log(dataAboutMe);
- */
-
-/* 
-const elementAboutMe = document.querySelector('#aboutMe');
-const aboutParagraph = document.createElement('p');
-const aboutDiv = document.createElement('div');
-aboutDiv.classList.add('#headshotContainer');
-
-aboutImage.src = dataAboutMe.headShot;
-aboutImage.alt = "Headshot of me";
-aboutDiv.append(aboutImage);
-elementAboutMe.append(aboutParagraph);
-
- */
-
-
-
-
-//console.log(aboutMeFragment);
 
 
 
